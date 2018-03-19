a52f4a5420bc79e9f3ffe9e6f1acb795e1ae6c84
Adam Godor <adam.godor@gmail.com>
2018-03-19 11:47:27 +0100

    codebase

--- src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
+++ src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
@@ -1,4 +1,12 @@
 package com.codecool.enterprise;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RatingMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RatingMicroserviceApplication.class, args);
+    }
 }
