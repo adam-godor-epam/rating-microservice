04cfa5ea3ca2bfd7fe02209a062bc9f5aa42b722
Adam Godor <adam.godor@gmail.com>
2018-03-20 10:27:42 +0100

    Procfile changed

--- src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
+++ src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RatingMicroserviceApplication {
     public static void main(String[] args) {
+        //web: java -cp RatingMicroService-1.0-SNAPSHOT.jar com.codecool.enterprise.RatingMicroserviceApplication
         SpringApplication.run(RatingMicroserviceApplication.class, args);
     }
 }
