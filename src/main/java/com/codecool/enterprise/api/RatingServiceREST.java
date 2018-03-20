a770010d6140db6a31e876428e55c5921c504ce4
Dani Knoll <knoll.dani@gmail.com>
2018-03-20 14:46:57 +0100

    Json turned to Gson, added save ratingREST
    Untested.

--- src/main/java/com/codecool/enterprise/api/RatingServiceREST.java
+++ src/main/java/com/codecool/enterprise/api/RatingServiceREST.java
@@ -2,15 +2,18 @@ package com.codecool.enterprise.api;
 
 import com.codecool.enterprise.model.Rating;
 import com.codecool.enterprise.service.RatingService;
-import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import com.google.gson.*;
 
 import java.util.List;
 
 @RestController
 public class RatingServiceREST {
+    private String inputUrl = "http://...";
 
     private RatingService ratingService;
 
@@ -26,16 +29,39 @@ public class RatingServiceREST {
 
     @GetMapping("/rating/user/{id}/avg")
     public ResponseEntity getUserRateAvg(@PathVariable("id") int id) {
-        JSONObject avg = new JSONObject();
-        avg.put("avg", ratingService.getAverageRatingBySellerId(id));
+        Gson avg = new Gson();
+        avg.toJson(ratingService.getAverageRatingBySellerId(id));
         return new ResponseEntity(avg, HttpStatus.OK);
     }
 
     @GetMapping("/rating/user/{id}/count")
     public ResponseEntity getUserRateCount(@PathVariable("id") int id) {
-        JSONObject avg = new JSONObject();
-        avg.put("count", ratingService.getRatingCountBySellerId(id));
-        return new ResponseEntity(avg, HttpStatus.OK);
+        Gson count = new Gson();
+        count.toJson(ratingService.getRatingCountBySellerId(id));
+        return new ResponseEntity(count, HttpStatus.OK);
+    }
+
+    @PostMapping("/rating")
+    public ResponseEntity saveUserRate() {
+        String inputString;
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            inputString = restTemplate.getForEntity(inputUrl, String.class).getBody();
+        } catch (RestClientException e) {
+            System.out.println("Provider API is not running");
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        Gson input = new Gson();
+        input.toJson(inputString);
+
+        Rating rating = new Rating(input.fromJson("sellerId", int.class),
+                input.fromJson("buyerId", int.class),
+                input.fromJson("productId", int.class),
+                input.fromJson("stars", int.class),
+                input.fromJson("review", String.class));
+        ratingService.saveRating(rating);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
