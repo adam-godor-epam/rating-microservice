874f6cdc17f31e8a502aa17d107b5588592ca07c
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2018-03-20 09:48:12 +0100

    rating model done

--- src/main/java/com/codecool/enterprise/model/Rating.java
+++ src/main/java/com/codecool/enterprise/model/Rating.java
@@ -1,17 +1,42 @@
 package com.codecool.enterprise.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Rating {
 
+    @Id
     private int id;
 
+    @NotNull
     private int sellerId;
 
+    @NotNull
     private int buyerId;
 
-    private int start;
+    @NotNull
+    private int productId;
 
-    private  int productId;
+    @NotNull
+    @Min(1)
+    @Max(5)
+    private int stars;
+
+    private String review;
+
+    public Rating() {
+    }
+
+    public Rating(int id, int sellerId, int buyerId, int productId, int stars, String review) {
+        this.id = id;
+        this.sellerId = sellerId;
+        this.buyerId = buyerId;
+        this.productId = productId;
+        this.stars = stars;
+        this.review = review;
+    }
 }
