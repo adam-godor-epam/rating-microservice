4eb28e2c547484fa00283aff0c57312703587a9d
Adam Godor <adam.godor@gmail.com>
2018-03-21 13:02:49 +0100

    annotation added to Main class

--- src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
+++ src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
@@ -2,9 +2,11 @@ package com.codecool.enterprise;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class RatingMicroserviceApplication {
     public static void main(String[] args) {
         //web: java -cp RatingMicroService-1.0-SNAPSHOT.jar com.codecool.enterprise.RatingMicroserviceApplication
