da70c236dfc66864e3a47e2040a11243d1a5ebf2
Adam Godor <adam.godor@gmail.com>
2018-03-21 14:56:26 +0100

    heroku deploy ready

--- src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
+++ src/main/java/com/codecool/enterprise/RatingMicroserviceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAutoConfiguration
 public class RatingMicroserviceApplication {
     public static void main(String[] args) {
-        //web: java -cp RatingMicroService-1.0-SNAPSHOT.jar com.codecool.enterprise.RatingMicroserviceApplication
         SpringApplication.run(RatingMicroserviceApplication.class, args);
     }
 }
--- src/main/java/com/codecool/enterprise/controller/RatingController.java
+++ src/main/java/com/codecool/enterprise/controller/RatingController.java
@@ -1,13 +1,16 @@
 package com.codecool.enterprise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RatingController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
+    //@ResponseBody
     public String renderMain(){
         return "main";
     }
